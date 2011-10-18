73cd05eaaba78d7668f9b144fcbe7bb086c701e0
Joon Lee <seouri@gmail.com>
2011-10-18 10:52:22 -0400

    add Download link

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Geneset::Application.routes.draw do
   match 'meshcomplete-update' => 'genes#top'
   match "/pubmed/:gene_id/:term/:rettype" => "genes#pubmed", :as => "pubmed"
   match '/about' => "page#about", :as => "about"
+  match '/download' => "page#download", :as => "download"
   root :to => "page#home"
 end
